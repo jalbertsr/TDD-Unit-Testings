4d576b192c0c246e50e0d98e4d83c47fa74549c0
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-07-08 02:11:53 +0200

    TDD-anagram finished

--- TDD-cyberDojo/spec/anagramsSpec.js
+++ TDD-cyberDojo/spec/anagramsSpec.js
@@ -1,63 +1,41 @@
-describe("function fizzBuzz", function(){
-  it("fizzBuzz should exists", function(){
-    expect(fizzBuzz).toBeDefined();
-  })
-  
-  it('fizzBuzz should a function', function(){
-    expect(typeof fizzBuzz).toBe('function')
-  })
-  
-  it('fizzBuzz should return an array', function(){
-    expect(Array.isArray(fizzBuzz())).toBe(true)
+describe("function fizzBuzz", function() {
+  it("should exist", function() {
+    expect(anagram).toBeDefined();
   })
 
-  it('fizzBuzz should return a length array of 100', function(){
-    expect(fizzBuzz().length).toBe(100)
+  it("should be a function ", function() {
+    expect(typeof anagram).toBeDefined();
   })
 
-  it('fizzBuzz never should return numbers that are divisibles by 3', function(){
-    expect(fizzBuzz().includes(3)).toBe(false)
+  it("should return an array ", function() {
+    expect(Array.isArray(anagram("biro"))).toBeTruthy();
   })
 
-  it('fizzBuzz should return fizz if numbers are divisibles by 3', function(){
-    var array = fizzBuzz()
-    expect(array[2]).toBe('fizz')
-    
-  })
-  it('fizzBuzz never should return numbers that are divisibles by 5', function(){
-    expect(fizzBuzz().includes(5)).toBe(false)
+  it("should return an array of strings", function() {
+
+    expect().toEqual()
   })
 
-  it('fizzBuzz should return buzz if numbers are divisibles by 3', function(){
-    var array = fizzBuzz()
-    expect(array[4]).toBe('buzz')
-    
+  it("should return an array without numbers", function() {
+    function checkArray(word) {
+      return typeof word === 'number'
+    }
+    var toBeNumber = anagram("biro").some(checkArray)
+    expect(toBeNumber).toBeFalsy()
   })
 
-  it('fizzBuzz never should return a 15 on the array', function(){
-    expect(fizzBuzz().includes(15)).toBe(false)
+  it("should return 24 combinations for word 'biro' ", function() {
+    var anagramLength = anagram("biro").length
+    expect(anagramLength).toEqual(24)
   })
 
-  it('fizzBuzz should return fizzBuzz if numbers are divisibles by 15', function(){
-    var array = fizzBuzz()
-    expect(array[29]).toBe('fizzBuzz')
-    
+  it("should has 'ribo' inside the returned array ", function() {
+    var ribo =  anagram("biro").includes('ribo')
+    expect(ribo).toBeTruthy()
   })
 
-  it('fizzBuzz never should return numbers that are divisibles by 3 or 5', function(){
-    expect(fizzBuzz().includes(3 || 5)).toBe(false)
-  })
-
-
-  it('fizzBuzz should return fizz if numbers has a 3', function(){
-    var array = fizzBuzz()
-    expect(array[34]).toBe('fizz')
-    
-  })
-
-  it('fizzBuzz should return fizzBuzz if numbers has a 5', function(){
-    var array = fizzBuzz()
-    expect(array[49]).toBe('buzz')
-    
+  it("should has 'orib' inside the returned array ", function() {
+    var orib =  anagram("ribo").includes('orib')
+    expect(orib).toBeTruthy()
   })
 })
--- TDD-cyberDojo/src/anagrams.js
+++ TDD-cyberDojo/src/anagrams.js
@@ -1,10 +1,30 @@
+function swap(array, i, j) { 
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+function permutations(arrayWithLetters, start, end) {
+  var result = [];
+  if (start === end) {
+    return [arrayWithLetters.join('')];
+  }
+  for (var i = start; i <= end; i++) { 
+    swap(arrayWithLetters, start, i);
+    var tempResult = permutations(arrayWithLetters, start + 1, end); //appplaying recursion
+    result = result.concat(tempResult);
+    swap(arrayWithLetters, start, i);
+  }
+  return result;
+}
+
+/*main function*/
+function anagram(word) {
+
+  var letters = word.split('')
+  var len = word.length-1
+
+  return permutations(letters, 0, len)
+}
 
 
-
-
-
-
-
-
-
-
+ 
