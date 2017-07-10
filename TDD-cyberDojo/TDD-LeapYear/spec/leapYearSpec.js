b6a3a7e2d66a18d8273fc11594791649e6e0fece
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-07-10 10:43:28 +0200

    improved leapYearSpec description test

--- TDD-cyberDojo/TDD-LeapYear/spec/leapYearSpec.js
+++ TDD-cyberDojo/TDD-LeapYear/spec/leapYearSpec.js
@@ -11,12 +11,14 @@ describe("function leapYear", function() {
     expect(typeof leapYear(2003)).toBe('boolean')
   })
 
-  it("should return true for year 1996 ", function() {
-    expect(leapYear(1996)).toBeTruthy();
-  })
+  describe("should return true for leap years", function() {
+      it("should return true for year 1996 ", function() {
+        expect(leapYear(1996)).toBeTruthy();
+      })
 
-  it("should return false for year 1997 ", function() {
-    expect(leapYear(1997)).toBeFalsy();
+    it("should return false for year 1997 ", function() {
+      expect(leapYear(1997)).toBeFalsy();
+    })
   })
 
 })
